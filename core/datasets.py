7ba14891c76011e2e294ecb4bbdc854dda85b1ba
Mihai Balica <mihai.balica@123forbuilder.com>
2021-12-19 22:02:03 +0200

    1

--- core/datasets.py
+++ core/datasets.py
@@ -160,10 +160,10 @@ class FlyingThings3D(FlowDataset):
 class KITTI(FlowDataset):
     def __init__(self, aug_params=None, split='training', root='/content/datasets/KITTI'):
         super(KITTI, self).__init__(aug_params, sparse=True)
-        if split == 'testing':
-            self.is_test = True
+        # if split == 'testing':
+        #     self.is_test = True
 
-        root = osp.join(root, split)
+        # root = osp.join(root, split)
         images1 = sorted(glob(osp.join(root, 'image_02/data/*.png')))
         images2 = sorted(glob(osp.join(root, 'image_02/data/*.png')))
 
@@ -177,8 +177,8 @@ class KITTI(FlowDataset):
             self.extra_info += [ [frame_id] ]
             self.image_list += [ [img1, img2] ]
 
-        if split == 'training':
-            self.flow_list = sorted(glob(osp.join(root, 'flow_occ/*_10.png')))
+        # if split == 'training':
+        #     self.flow_list = sorted(glob(osp.join(root, 'flow_occ/*_10.png')))
 
 
 class HD1K(FlowDataset):
