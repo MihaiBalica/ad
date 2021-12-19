9f6f832cdef31904c0ee99dabb4f1fb868e4beee
Mihai Balica <mihai.balica@123forbuilder.com>
2021-12-19 22:37:55 +0200

    1

--- run_inference.py
+++ run_inference.py
@@ -16,37 +16,6 @@ from utils import frame_utils
 from utils.utils import InputPadder, forward_interpolate
 
 
-@torch.no_grad()
-def create_sintel_submission(model, warm_start=False, output_path='sintel_submission'):
-    """ Create submission for the Sintel leaderboard """
-    model.eval()
-    for dstype in ['clean', 'final']:
-        test_dataset = datasets.MpiSintel(split='test', aug_params=None, dstype=dstype)
-
-        flow_prev, sequence_prev = None, None
-        for test_id in range(len(test_dataset)):
-            image1, image2, (sequence, frame) = test_dataset[test_id]
-            if sequence != sequence_prev:
-                flow_prev = None
-
-            padder = InputPadder(image1.shape)
-            image1, image2 = padder.pad(image1[None].to(f'cuda:{model.device_ids[0]}'), image2[None].to(f'cuda:{model.device_ids[0]}'))
-
-            flow_low, flow_pr = model.module(image1, image2, iters=32, flow_init=flow_prev, test_mode=True)
-            flow = padder.unpad(flow_pr[0]).permute(1, 2, 0).cpu().numpy()
-
-            if warm_start:
-                flow_prev = forward_interpolate(flow_low[0])[None].cuda()
-
-            output_dir = os.path.join(output_path, dstype, sequence)
-            output_file = os.path.join(output_dir, 'frame%04d.flo' % (frame+1))
-
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
-
-            frame_utils.writeFlow(output_file, flow)
-            sequence_prev = sequence
-
 
 @torch.no_grad()
 def create_kitti_submission(model, output_path='kitti_submission'):
@@ -58,24 +27,23 @@ def create_kitti_submission(model, output_path='kitti_submission'):
         os.makedirs(output_path)
 
     for test_id in range(len(test_dataset)):
-        # image1, image2, (frame_id, ) = test_dataset[test_id]
-        image1, image2 = test_dataset[test_id]
-        image1 = image1[None].cuda()
-        image2 = image2[None].cuda()
-
+        image1, image2, (frame_id, ) = test_dataset[test_id]
         padder = InputPadder(image1.shape, mode='kitti')
-        image1, image2 = padder.pad(image1, image2)
+        image1, image2 = padder.pad(image1[None].to(f'cuda:{model.device_ids[0]}'), image2[None].to(f'cuda:{model.device_ids[0]}'))
 
-        flow_pr = model(image1, image2, iters=24, test_mode=True)
-        flow = padder.unpad(flow_pr[0]).cpu()
+        flow_pr = model.module(image1, image2)
+        flow = padder.unpad(flow_pr[0]).permute(1, 2, 0).cpu().numpy()
 
-        output_filename = os.path.join(output_path, test_id)
+        output_filename = os.path.join(output_path, frame_id)
         frame_utils.writeFlowKITTI(output_filename, flow)
+
+
 
 
 @torch.no_grad()
 def validate_kitti(model, iters=6):
     """ Peform validation using the KITTI-2015 (train) split """
+    output_path='kitti_submission'
     model.eval()
     val_dataset = datasets.KITTI(split='training')
 
@@ -90,6 +58,8 @@ def validate_kitti(model, iters=6):
 
         flow_pr = model(image1, image2, iters=iters, test_mode=True)
         flow = padder.unpad(flow_pr[0]).cpu()
+        output_filename = os.path.join(output_path, val_id)
+        frame_utils.writeFlowKITTI(output_filename, flow)
 
         epe = torch.sum((flow - flow_gt)**2, dim=0).sqrt()
         mag = torch.sum(flow_gt**2, dim=0).sqrt()
@@ -126,10 +96,10 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load(args.model))
 
     model.cuda()
-    # model.eval()
+    model.eval()
 
     # create_sintel_submission(model.module, warm_start=True)
-    create_kitti_submission(model.module)
+    # create_kitti_submission(model.module)
 
     with torch.no_grad():
         if args.dataset == 'chairs':
